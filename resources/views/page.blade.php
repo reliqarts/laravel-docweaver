51af7e06718ed94afe07b9420047df919ae9d2c3
ReliQ <versatile.jamrok@gmail.com>
2017-10-04 19:53:09 -0500

    Updates Sidebar - Implements sidebare collapse/expand all - Collapse states are remembered - Sidebar links are now aware of current page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,8 +2,7 @@
 
 @section('doc-weaver-content')
 <section class="sidebar">
-	<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=laravelcom" id="_carbonads_js"></script>
-	<small><a href="#" id="doc-expand" style="font-size: 9px; color: #525252;">▶</a></small>
+	<small><a href="#" id="doc-expand" class="doc-weaver-doc-expand">▶</a></small>
 	{!! $index !!}
 </section>
 
