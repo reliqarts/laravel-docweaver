dc4a47dd17318d34ef6ea3a63b4bbf1a1e50d503
reliq <reliq@reliqarts.com>
2023-03-08 10:26:41 +0100

    drop laravel 8 support

--- ecs.php
+++ ecs.php
@@ -2,15 +2,11 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::PSR_12);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::LINE_ENDING, "\n");
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([SetList::CLEAN_CODE, SetList::PSR_12]);
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+    $ecsConfig->lineEnding("\n");
 };
